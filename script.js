0eec6aa83beabdc3d78788502101a7d3f707f991
Oliver Trinidad <ocmtrinidad@gmail.com>
2023-08-30 10:36:22 +0800

    removed unwanted code and created reset button

--- script.js
+++ script.js
@@ -5,30 +5,12 @@ const gameBoardModule= (() => {
     const createPlayer = (name, marker) => {
         return {name, marker};
     };
+    // make buttons to pick marker
+    const playerOne= createPlayer("Player One", "X");
+    const playerTwo= createPlayer("Player Two", "O");
 
-    let marker;
-    const playX= document.querySelector(".playX");
-    const playO= document.querySelector(".playO");
-    const or= document.querySelector(".or");
-    playX.addEventListener("click", () => {
-        container.setAttribute("style", "display:grid; border: 2px solid black;");
-        const playerOne = createPlayer("Player One", "X");
-        const playerTwo = createPlayer("Player Two", "O");
-        marker = playerOne.marker;
-        playX.setAttribute("style", "display:none;");
-        playO.setAttribute("style", "display:none;");
-        or.setAttribute("style", "display:none;");
-    });
-    playO.addEventListener("click", () => {
-        container.setAttribute("style", "display:grid; border: 2px solid black;");
-        const playerOne = createPlayer("Player One", "O");
-        const playerTwo = createPlayer("Player Two", "X");
-        marker = playerOne.marker;
-        playX.setAttribute("style", "display:none;");
-        playO.setAttribute("style", "display:none;");
-        or.setAttribute("style", "display:none;")
-    });
-
+    let marker= playerOne.marker;
+    
     const displayBoard= () => {
         gameBoard.forEach((item, index) => {
             const squares= document.createElement("div");
@@ -37,6 +19,8 @@ const gameBoardModule= (() => {
             container.appendChild(squares);
             squares.addEventListener("click", placeMarker);
         });
+        const reset= document.querySelector(".reset");
+        reset.addEventListener("click", resetGame);
     };
 
     const placeMarker = (e) => { 
@@ -45,10 +29,10 @@ const gameBoardModule= (() => {
         e.target.appendChild(cell);
         (marker == "X") ? marker = "O" : marker = "X";
         e.target.removeEventListener("click", placeMarker);
-        // gameBoard not updating
-        // stuck in gameBoard[0];
-        gameBoard.splice(e.target, 1, marker);
-        console.log(gameBoard);
+    };
+
+    const resetGame = () =>{
+        location.reload();
     };
 
     return {displayBoard};
